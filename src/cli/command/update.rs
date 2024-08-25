1c4b33fc0e4695cabc468cc49f0353b71e057a88
Richard Lyon <richlyon@mac.com>
2024-08-25 09:13:59 -0400

    add progress bar to update command

--- src/cli/command/update.rs
+++ src/cli/command/update.rs
@@ -1,8 +1,12 @@
 //! Update datafiles command
+//!
+//! Downloads the latest datafiles from the CEDA API.
 
 use crate::ceda::CedaClient;
 use crate::datastore;
 use crate::error::AppError as Error;
+use indicatif::{ProgressBar, ProgressStyle};
+use std::time::Duration;
 
 pub async fn update() -> Result<(), Error> {
     let datastore = datastore::DataStore::new();
@@ -10,6 +14,7 @@ pub async fn update() -> Result<(), Error> {
     let mut all_station_links: Vec<String> = Vec::new();
 
     let region_links = client.get_region_links().await.map_err(|_| Error::GenericError)?;
+
 
     // Get the stations for each region
     for link in region_links.iter().take(1) { // FIXME: remove .take(1) to get all regions
@@ -19,15 +24,38 @@ pub async fn update() -> Result<(), Error> {
 
     println!("Downloading to {:?}", datastore.root);
 
+    let total_stations = all_station_links.len();
+
+    let pb = create_progress_bar(total_stations as u64, "Fetching station data".to_string());
+
     // Download the data for each station
-    for station_link in all_station_links.into_iter().take(1) {
+    for station_link in all_station_links.into_iter().take(5) {
         let data_links = client.get_data_links(&station_link).await.map_err(|_| Error::GenericError)?;
         client.download_csv(&data_links.capability, &datastore.capability_dir()).await.map_err(|_| Error::GenericError)?;
         for link in data_links.data.iter() {
             client.download_csv(link, &datastore.rawdata_dir()).await.map_err(|_| Error::GenericError)?;
         }
+
+        pb.inc(1);
     }
+
+    pb.finish_with_message("Finished fetching station data");
 
     Ok(())
 }
 
+
+pub fn create_spinner(message: String) -> ProgressBar {
+    let bar = ProgressBar::new_spinner().with_message(message);
+    bar.enable_steady_tick(Duration::from_millis(100));
+
+    bar
+}
+
+pub fn create_progress_bar(size: u64, message: String) -> ProgressBar {
+    ProgressBar::new(size).with_message(message).with_style(
+        ProgressStyle::with_template("[{eta_precise}] {bar:40.cyan/blue} {msg}")
+            .unwrap()
+            .progress_chars("##-"),
+    )
+}
